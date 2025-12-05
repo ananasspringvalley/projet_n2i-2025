7eb802123702546a4b8ca302d4912223c3bd9a8b
axel5564 <axel.robic@etu.iut-tlse3.fr>
2025-12-05 05:04:36 +0100

    ajout du contenue dans div

--- src/routes/3d/+page.svelte
+++ src/routes/3d/+page.svelte
@@ -11,7 +11,13 @@
         <div>
             <h1>Obsolescence programée</h1>
             <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis amet magnam commodi quos fugiat laboriosam soluta, incidunt velit quidem repudiandae, deleniti esse quia explicabo deserunt sint. Eum ab possimus hic.            </p>
+                la plus part des appareille électroniques produit par les grandes corporations sont conçu
+                dans la vision d'une durai de vie courte et programmer de tel sorte à réduire leur
+                duré de vie. Cela est appeler l'obsolescence programmée, cette pratique peut se faire de
+                plusieurs moyennes, que cela soit par des logiciel qui demande des composants neuf ou encore
+                des modifications sur les composants pour rendre la réparation plus difficile.
+
+            </p>
         </div>
         <div class="sceneDroite">
             <Scene>
@@ -23,7 +29,15 @@
         <div>
             <h1>Pollution</h1>
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam reiciendis ab officia ipsum. Totam saepe soluta eum neque molestiae at architecto labore perferendis? Fuga ratione ullam quo nisi dolorum expedita!
+                La production des composant électroniques représente une grande partie de la pollution
+                produit dans le monde, cela a un lien avec la récolte et traitement des matières
+                première mais aussi avec la consommation des processus intermédiaire et de leur
+                utilisation. Pour information la production d'un ordinateur consomme 800 kg de
+                matières premières et leur utilisation régulière représente 125 kg de CO² par an.
+                de plus les serveurs représente une consommation de 3000L d'eau par internaute et par
+                ans.
+
+
             </p>
         </div>
         <div class="sceneGauche">
@@ -34,9 +48,14 @@
     </div>
     <div class ="containerDroite">
         <div>
-            <h1>JSP encore</h1>
+            <h1>conclusion</h1>
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam reiciendis ab officia ipsum. Totam saepe soluta eum neque molestiae at architecto labore perferendis? Fuga ratione ullam quo nisi dolorum expedita!
+                Cependant malgré tous les problèmes causés par les grandes corporations il y a
+                toujours une solution, et c'est les logiciel open source et les système
+                d'exploitation peu couteux en termes de puissances de calcule comme linux.
+                on peut encore changer nos habitudes pour passer à des méthodes libres de
+                droit et faire aux grandes corporations.
+
             </p>
         </div>
         <div class="sceneDroite">
