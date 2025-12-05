74074bb7515e2a9fcbe624746327958a4916d50e
StepanTheGreat <117771383+StepanTheGreat@users.noreply.github.com>
2025-12-05 05:32:43 +0100

    Supprimer rapier

--- src/routes/3d/scenes/Scene.svelte
+++ src/routes/3d/scenes/Scene.svelte
@@ -1,22 +1,19 @@
 <script lang="ts">
-    import { World } from '@threlte/rapier';
-    
     import { Canvas, extend, T} from '@threlte/core'
     import { CameraControls, TransformControls } from '@threlte/extras'
+    
     extend({TransformControls});
 </script>
 
 <Canvas>
-    <World>
-        <T.AmbientLight color="#FFFFFF" intensity={1}></T.AmbientLight>
+    <T.AmbientLight color="#FFFFFF" intensity={1}></T.AmbientLight>
 
-        <T.PerspectiveCamera
-        makeDefault
-        position={[-10, 8, -10]}
-        >
-            <CameraControls enableZoom={false}/>
-        </T.PerspectiveCamera>
-        
-        <slot></slot>
-    </World>
+    <T.PerspectiveCamera
+    makeDefault
+    position={[-10, 8, -10]}
+    >
+        <CameraControls enableZoom={false}/>
+    </T.PerspectiveCamera>
+    
+    <slot></slot>
 </Canvas>
