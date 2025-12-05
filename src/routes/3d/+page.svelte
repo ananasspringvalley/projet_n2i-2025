95b56b58d84dec8b417fd56d1bf406288428720c
StepanTheGreat <117771383+StepanTheGreat@users.noreply.github.com>
2025-12-05 02:04:30 +0100

    On a perdu du temps

--- src/routes/3d/+page.svelte
+++ src/routes/3d/+page.svelte
@@ -1,17 +1,26 @@
 <script lang="ts">
     import { Canvas } from '@threlte/core'
-    import Scene from './Scene.svelte';
-
-    let scene: Scene;
+    import Update from './scenes/Update.svelte';
 </script>
 
 <main>
-    <Canvas>
-        <Scene bind:this={scene}></Scene>
-    </Canvas>
+
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam dolor expedita, a dignissimos veniam rem suscipit debitis tenetur ab commodi nesciunt, iusto sint eius? Atque, nihil aliquid. Asperiores, quibusdam unde.
+    <div class="scene">
+        <Canvas>
+            <Update/>
+        </Canvas>
+    </div>
 </main>
 
 <style>
+    .scene {
+        width: 360px;
+        height: 300px;
+
+        border: 2px solid black;
+    }
+
     main {
         height: 100%;
         overflow: hidden;
