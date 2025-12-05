49bdf73c6c25d09e00f10d80030dbbbfbf104c65
Camillegrc31 <garcia.camille@protonmail.com>
2025-12-05 05:02:05 +0100

    penguin

---
+++ src/routes/3d/scenes/linux.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import { World } from '@threlte/rapier';
+    
+    import { Linux, extend, T} from '@threlte/core'
+    import { CameraControls, TransformControls } from '@threlte/extras'
+    extend({TransformControls});
+</script>
+
+<Linux>
+    <World>
+        <T.AmbientLight color="#FFFFFF" intensity={1}></T.AmbientLight>
+
+        <T.PerspectiveCamera
+        makeDefault
+        position={[-10, 8, -10]}
+        >
+            <CameraControls enableZoom={false}/>
+        </T.PerspectiveCamera>
+        
+        <slot></slot>
+    </World>
+</Linux>
