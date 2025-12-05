fc3bf534935def681e7405b79267c6baf79e5124
Camillegrc31 <garcia.camille@protonmail.com>
2025-12-05 02:01:28 +0100

    suppression application

--- src/routes/3d/Screen.svelte
+++ src/routes/3d/Screen.svelte
@@ -5,17 +5,9 @@
 </script>
 
 <div id="screen">
-    <App></App>
+ 
 </div>
-<!-- 
+
 <style>
-    #screen {
-        width: 200px;
-        height: 80px;
 
-        background-color: blue;
-        overflow: hidden;
-    }
-
-    
-</style> -->
+</style> 
